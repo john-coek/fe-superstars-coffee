f1472e1a84b9461447f95fde7cd4e3db0e31f3f2
Azhar Syahid <rahza8096@gmail.com>
2026-05-09 19:34:44 +0700

    Update minor at index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,7 +28,7 @@ const router = createRouter({
     ],
 });
 
-router.beforeEach(async (to, from, next)=> {
+router.beforeEach(async (to, _from, next)=> {
   const auth = useAuthStore()
 
   if(auth.isAuthenticated && !auth.user){
